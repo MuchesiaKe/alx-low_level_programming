f53db21f1da6477f362e59c052b9e59a66abc26e
Muchesia <muchesiawallace@gmail.com>
2022-09-14 19:42:37 -0700

    Initial commit with README.md and first task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *prints - Prints _putchar followed by a new line
+ *Return:
+#include "main.h"
+/**
+ * main - prints _putchar, followed by a new line
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char oli[] = "_putchar";
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(oli[c]);
+	}
+	_putchar('\n');
+	return (0);
+} Retruns 0
+ */
+
+int prints(void)
+{
+	_putchar("_putchar\n");
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+C- Functions, nested loops
+Projects for the ALX Function and nested loops projects
