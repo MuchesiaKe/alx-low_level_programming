ed1b3e47b85d6b0b9d12526f7defb35210e60445
MuchesKe <muchesiawallace@gmail.com>
2022-09-20 05:41:57 +0300

    Program to find length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_strlen - returns length of string
+*
+*@s: string to be returned length
+*
+*Return: Length of the string
+*/
+
+int _strlen(char *s)
+{
+	int i, len;
+
+	for (i = 0; s[i] != '\0'; i++)
+		len = i;
+	return (len);
+}
