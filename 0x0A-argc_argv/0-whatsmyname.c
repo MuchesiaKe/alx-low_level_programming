b70f4e8613bea18b394613d87210fe982621f364
MuchesKe <muchesiawallace@gmail.com>
2022-10-05 08:38:18 +0300

    added _putchar file, created main.h and completed first task to print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*main- prints its name followed by a new line
+*@argc : number of arguements passed
+*@argv : an array of passed arguements
+*Return : always (0)
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
