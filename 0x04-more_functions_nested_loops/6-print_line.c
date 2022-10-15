5624eeafddc17fc678ef5eaa420fe4aa1487abf8
Muchesia <muchesiawallace@gmail.com>
2022-10-15 15:21:00 +0300

    draws straigth line in the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_line - draws straight line in the terminal
+*
+*@n: number of times _ is printed
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
