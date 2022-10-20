cdcb7a864f19f99cae82f7bf07ca8bcf87b72ff0
Muchesia <muchesiawallace@gmail.com>
2022-10-20 11:41:34 +0300

    added (*select_operation()) that accepts user choice for calculation

--- self_practice/function_pointers/3-array_pointer_functions.c
+++ self_practice/function_pointers/3-array_pointer_functions.c
@@ -57,6 +57,35 @@ float divide(float x, float y)
 }
 
 /**
+*select_operation - selects operation to be done
+*/
+float (*select_operation())(float, float)
+{
+	int option;
+
+	option = 0;
+	printf("Select operation:\n");
+	printf("1)Addition\n");
+	printf("2)Substraction\n");
+	printf("3)Multiplication\n");
+	printf("4)Division\n");
+	printf("Enter choice: ");
+
+	scanf("%d", &option);
+
+	if (option == 1)
+		return sum;
+	else if (option == 2)
+		return substract;
+	else if (option == 3)
+		return multiply;
+	else if (option == 4)
+		return divide;
+	else
+		return NULL;
+}
+
+/**
 *main- checks the code
 *
 *Return: Always (0)
@@ -64,29 +93,9 @@ float divide(float x, float y)
 
 int main(void)
 {
-	float (*calc[4])(float, float);
-	float addition, multiplication, substraction, division;
-	float a, b;
+	float (*operation)(float, float);
 
-	calc[0] = &sum;
-	calc[1] = &substract;
-	calc[2] = multiply;
-	calc[3] = &divide;
-	a = 33;
-	b = 7;
-	addition = (*calc[0])(a, b);
-	multiplication = calc[2](a, b);
-	substraction = (*calc[1])(a, b);
-	division = calc[3](a, b);
-
-	/**
-	for (i = 0; i < 4; i++)
-		printf("Values of calc[%d] is %f\n", i, *calc[i]);
-	*/
-	printf("Sum of %f and %f is %f\n", a, b, addition);
-	printf("Product of %f and %f is %f\n", a, b, multiplication);
-	printf("Substraction of %f and %f is %f\n", a, b, substraction);
-	printf("Division of %f and %f is %f\n", a, b, division);
-
+	operation = select_operation();
+	printf("Result = %f\n", operation(20, 5)); 
 	return (0);
 }
