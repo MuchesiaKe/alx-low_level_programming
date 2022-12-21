faf147d13a5672ca539786f165bbebdd5ed5ba4c
Muchesia <muchesia@ubuntu20.04LTS>
2022-12-21 15:28:56 +0300

    fixed Betty issues

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,7 @@
 /**
 *_strlen - returns length of a string
 *@s: the string
+*Return: returns length of the string
 */
 
 int _strlen(char *s)
@@ -8,7 +9,7 @@ int _strlen(char *s)
 	int i, count;
 
 	count = 0;
-	for(i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 		count++;
 	return (count);
 }
