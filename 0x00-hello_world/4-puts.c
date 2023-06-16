4bb01eb34cc8a5603fcf87fbdaa51bdbc4df0db2
Muchesia <muchesiawallace@gmail.com>
2023-06-16 09:53:34 +0300

    Modify code for correct output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
-/*
+/**
 *main - program entrypoint
 *Return: Always 0(success)
 **/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
