9ba4e7512ed4ab62c65fc5f56366afb0e0267bba
Muchesia <muchesiawallace@gmail.com>
2023-06-16 17:31:25 +0300

    Print all single digits of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - prints all single digit numbers of base 10
+*Return: Always (0)
+**/
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar('0' + x);
+	putchar('\n');
+	return (0);
+}
