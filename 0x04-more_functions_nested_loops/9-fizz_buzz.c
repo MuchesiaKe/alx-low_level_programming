44cc2fb277efbf26bb672d4f99023df55b6c2c70
Muchesia <muchesiawallace@gmail.com>
2023-06-22 21:43:17 +0300

    Ace Fizz-Buzz Test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*fizz_buzz - Fizz-Buzz test
+**/
+
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+}
---
+++ 0x04-more_functions_nested_loops/9-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*main - controls program execution
+*Return: 0 on success, non-zero on failure
+**/
+
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
