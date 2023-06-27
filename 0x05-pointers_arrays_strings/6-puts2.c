3351a9d13db0feb245a2c0520f31d5da17578140
Muchesia <muchesiawallace@gmail.com>
2023-06-27 05:54:33 +0300

    Fixed Betty, removed condition to add '\0'

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,7 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i +=2)
+	for (i = 0; str[i] != '\0'; i += 2)
 		_putchar(str[i]);
-	if (str[i] != '\0')
-		str[i++] = '\0';
 	_putchar('\n');
 }
