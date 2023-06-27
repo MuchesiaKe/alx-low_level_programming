65c09e1db6e572ecbbc285c9f47039e41db75dee
Muchesia <muchesiawallace@gmail.com>
2023-06-27 20:18:30 +0300

    Illustrate null pointer

---
+++ pointers_arrays_strings/null.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - illustrate null pointer
+*
+*Return: Always 0
+**/
+
+int main(void)
+{
+	int *ptr = NULL;
+
+	printf("The value of ptr is : %x\n", ptr);
+
+	return (0);
+}
