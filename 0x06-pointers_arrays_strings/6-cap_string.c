3ab0a6b446983769496a3986947d0289d8e8173a
Muchesia <muchesiawallace@gmail.com>
2023-06-29 21:04:05 +0300

    Change value with deferencing

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+/**
+*cap_string - capitalizes all words of a string
+*
+*@str : string to be capitalized
+*
+*Return: Pointer to the capitalized string
+**/
+
+char *cap_string(char *str)
+{
+	int i, j, size;
+	char sep[] = {' ', '\t', '\n', ',', ';', '.',
+	 '!', '?', '"', '(', '(', '{', '}'};
+
+	size = sizeof(sep) / sizeof(char);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (str[i] == sep[j])
+			{
+				str[++i] = str[++i] - 32;
+			}
+		}
+	}
+	return (str);
+}
