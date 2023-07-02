7d173900945ac765674bc25488ddb1cd1f69fc6c
Muchesia <muchesiawallace@gmail.com>
2023-07-02 20:26:44 +0300

    Fixes

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,7 +10,7 @@ void reverse_array(int *a, int n)
 	int i, j;
 	char temp;
 
-	for (i = 0, j = (n - 1); i < j; i++, j--)
+	for (i = 0, j = n - 1; i <= j; i++, j--)
 	{
 		temp = a[i];
 		a[i] = a[j];
