75b9038ca2f6f197646111512014750324f6cbf8
Muchesia <muchesiawallace@gmail.com>
2023-07-03 16:32:52 +0300

    Copy memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+/**
+*_memcpy - copies memory area
+*@dest : copy destination
+*@src : copy source
+*@n : size to copy
+*Return: pointer to dest
+**/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*dest++ = *src++;
+	return (0);
+}
