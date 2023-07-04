1796e5f3ca1992366b6d4a886af07c0516f2cd0e
Muchesia <muchesiawallace@gmail.com>
2023-07-04 05:08:02 +0300

    Stuck with _strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+*_strspn - gets length of a prefix string
+*@s : the string with substring prefix
+*@accept : string to check for bytes in prefix substring
+*Return: number of bytes found
+**/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count;
+	int i, j;
+
+	count = i = j = 0;
+	while (*s)
+	{
+		while (*accept)
+		{
+			j++;
+			printf("%c %c\n", s[i], accept[j]);
+			if (s[i] == accept[j])
+			{
+				count++;
+				continue;
+			}
+		}
+		i++;
+	}
+	return (count);
+}
