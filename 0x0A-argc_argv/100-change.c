13efaacc4663ae83fe9e19727a4fee50cdee9685
Muchesia <muchesiawallace@gmail.com>
2023-07-08 05:59:47 +0300

    Change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+/**
+*main - prints the minimum number of coins to make change
+*@argc : argument counter
+*@argv : argument vector
+*Return: 0 for success, 1 for error
+**/
+
+int main(int argc, char **argv)
+{
+	int change, i;
+	
+	if (argc == 2)
+	{
+		change = atoi(argv[0]);
+		if (change < 0)
+			while (change)
+			{
+
+				if (change > 25)
+					change %=
+				if (change > 10)
+					change %=
+				if (change > )
+					change %=
+				if (change > )
+					change %=
+			}
+		else
+		{
+			printf("0\n");
+		}
+	}
+	else
+	{
+		printf("Error\n");
+		return (0);
+	}
+}
