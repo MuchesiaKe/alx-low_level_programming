6f9b7d1258098d6f0eb5ab82e4c5d4051d73fbe5
Muchesia <muchesiawallace@gmail.com>
2023-07-11 21:28:12 +0300

    Aced the task. I'm a fucking starboy

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+*argstostr - concatenates all the arguments, each
+*argument followed by a \n
+*@ac : argument counter
+*@av : argument vector
+*Return: pointer to new string, NULL on failure
+**/
+
+char *argstostr(int ac, char **av)
+{
+	char *av_mem;
+	int len, i, j, x;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	x = len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			len++;
+		}
+		len++;
+	}
+	av_mem = malloc((sizeof(*av) * len) + 1);
+	if (av_mem == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			av_mem[x++] = av[i][j];
+		av_mem[x++] =  '\n';
+	}
+	av[x] = '\0';
+	return (av_mem);
+}
