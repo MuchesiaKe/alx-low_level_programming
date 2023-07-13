da50efe8662762249a0327e5761fc90eeaa1dd16
Muchesia <muchesiawallace@gmail.com>
2023-07-13 19:12:16 +0300

    Add memory allocation failure handler

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,6 +24,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s2[i] != '\0' && i < n; i++)
 		size++;
 	concat_mem = malloc(sizeof(*s1) * size);
+	if (concat_mem == NULL)
+		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		concat_mem[x++] = s1[i];
 	for (i = 0; s2[i] != '\0' && i < n; i++)
