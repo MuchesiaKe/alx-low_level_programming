3562b62574fec5da520559f7515ebed49319ac95
Muchesia <muchesiawallace@gmail.com>
2023-07-13 22:52:27 +0300

    Added condition for nmemb and size == 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,10 +12,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *arr_mem;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 	arr_mem = malloc(nmemb * size);
-	if (arr_mem == NULL || size == 0)
+	if (arr_mem == NULL)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
 		arr_mem[i] = '\0';
-	return ((void*)arr_mem);
+	return (arr_mem);
 }
