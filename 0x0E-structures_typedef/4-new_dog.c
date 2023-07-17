3e7fa5b44de2ae044f5291be4a87743585b40d74
Muchesia <muchesiawallace@gmail.com>
2023-07-17 23:21:17 +0300

    Returns a struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+*new_dog - creates new dog
+*@name	:  name of the dog
+*@age : age of the dog
+*@owner : owner of the dog
+*Return: pointer to the struct
+**/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *created_dog;
+
+	created_dog = malloc(sizeof(dog_t));
+	created_dog->name = name;
+	created_dog->owner = owner;
+	created_dog->age = age;
+	return (created_dog);
+}
