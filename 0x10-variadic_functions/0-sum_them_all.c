1223a66e6b89a680e74481b4bde83604989c6840
Muchesia <muchesiawallace@gmail.com>
2023-07-20 21:24:44 +0300

    Returns the sum of its parameters

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+*sum_them_all - returns the sum of all its parameters
+*@n: number of integers
+*Return: sum of integers
+**/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum, arg;
+	va_list sum_arg;
+
+	sum = 0;
+	va_start(sum_arg, n);
+	for (i = 0; i < n; i++)
+	{
+		arg = va_arg(sum_arg, int);
+		sum += arg;
+	}
+	va_end(sum_arg);
+	return (sum);
+}
