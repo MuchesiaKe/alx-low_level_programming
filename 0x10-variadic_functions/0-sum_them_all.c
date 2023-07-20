4ef64927bc0d48d9785291f742d58ed3431a05d9
Muchesia <muchesiawallace@gmail.com>
2023-07-20 21:25:47 +0300

    Added condition for n == 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,6 +11,8 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i, sum, arg;
 	va_list sum_arg;
 
+	if (n == 0)
+		return (0);
 	sum = 0;
 	va_start(sum_arg, n);
 	for (i = 0; i < n; i++)
