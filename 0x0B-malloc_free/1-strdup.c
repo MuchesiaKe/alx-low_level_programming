30123fa8208e4d4c02f2203fe200af9ddb54b06b
Muchesia <muchesiawallace@gmail.com>
2023-08-07 08:57:23 +0300

    strdup implementation

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*_strdup - returns pointer to a newly allocated space in memory
+*which contains a copy of the string given as parameter
+*@str: string to store in allocatted memory
+*Return: pointer to the allocated memory
+**/
+
+char *_strdup(char *str)
+{
+	char *mem_alloc;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	mem_alloc = malloc(sizeof(i + 1));
+	if (mem_alloc == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		mem_alloc[i] = str[i];
+	}
+	mem_alloc[i] = '\0';
+	return (mem_alloc);
+}
