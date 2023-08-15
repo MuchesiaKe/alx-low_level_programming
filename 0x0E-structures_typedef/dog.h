ead145b08d1a7bcd829f4ac797cfe834b2c4c325
Muchesia <muchesiawallace@gmail.com>
2023-08-15 05:55:00 +0300

    Fixed betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+	#include <stdlib.h>
+	#include <stdio.h>
+
 /**
 *struct dog - dog information
 *@name: name of the dog
 *@age: age of the dog
 *@owner: dog owner
-
 **/
 
-	#include <stdlib.h>
-	#include <stdio.h>
+
 	struct dog
 	{
 		char *name;
