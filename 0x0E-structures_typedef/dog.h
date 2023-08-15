869073eb745f02369a683f33572056b1f69c396d
Muchesia <muchesiawallace@gmail.com>
2023-08-15 05:34:16 +0300

    Does not initialize the parameters

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,11 +6,16 @@
 *@name: name of the dog
 *@age: age of the dog
 *@owner: dog owner
+
 **/
+
+	#include <stdlib.h>
 	struct dog
 	{
 		char *name;
 		float age;
 		char *owner;
 	};
+
+	void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
