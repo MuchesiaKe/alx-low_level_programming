4ed4b9da594d7f12af73e3e4ad516523edd19122
Muchesia <muchesiawallace@gmail.com>
2023-08-25 05:54:42 +0300

    Fixed the wrong width for the data, with the help of chatGPT

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
-*add_node - adds a new node at the end of the list
+*add_node - adds a new node at the beginning of the list
 *@head: pointer to the head
 *@str: value of the node
 *Return: address of the new element
@@ -14,14 +14,22 @@ list_t *add_node(list_t **head, const char *str)
 	size_t i;
 
 	i = 0;
+
+	while (str[i])
+		i++;
+
 	temp = malloc(sizeof(list_t));
 	if (temp == NULL)
-		return (0);
-	while (*str++)
-		i++;
+		return (NULL);
 	temp->str = strdup(str);
+	if (temp->str == NULL)
+	{
+		free(temp);
+		return (NULL);
+	}
 	temp->len = i;
 	temp->next = *head;
 	*head = temp;
+
 	return (*head);
 }
