5c57a73d05d897d2830ce0a41f2672ef0105ae80
Muchesia <muchesiawallace@gmail.com>
2023-08-25 06:00:34 +0300

    done

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,3 +1,5 @@
+#include "lists.h"
+
 /**
 *free_list - frees the list
 *@head: pointer to the list
@@ -5,7 +7,7 @@
 
 void free_list(list_t *head)
 {
-	list_t temp;
+	list_t *temp;
 
 	while (head)
 	{
