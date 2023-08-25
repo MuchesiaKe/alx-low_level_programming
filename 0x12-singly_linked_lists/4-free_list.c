646fddf4d8f2319230add0110b752b971ee712f3
Muchesia <muchesiawallace@gmail.com>
2023-08-25 05:58:26 +0300

    Free linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,9 +5,13 @@
 
 void free_list(list_t *head)
 {
-	while (head->next)
+	list_t temp;
+
+	while (head)
 	{
-		free(head);
+		temp = head;
 		head = head->next;
+		free(temp->str);
+		free(temp);
 	}
 }
