3b0a6c813f17af1180ea19d7945747a03c39a427
Muchesia <muchesiawallace@gmail.com>
2023-08-28 22:39:37 +0300

    Add node at the end of the list. Segmenation fault!

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+*add_nodeint_end - adds new node at the of a list
+*@head: pointer to the list
+*@n: value for the new list
+*Return: address of the new element
+**/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *last_node, *temp;
+
+	temp = *head;
+	last_node = malloc(sizeof(listint_t));
+	if (last_node == NULL)
+		return (NULL);
+	last_node->n = n;
+	last_node->next = NULL;
+
+	while (temp->next)
+	{
+		temp = temp->next;
+	}
+	temp->next = last_node;
+	return (last_node);
+}
