9ff5e0ab9750478663421d70fb8cc22ec706717e
Muchesia <muchesiawallace@gmail.com>
2023-08-30 03:45:12 +0300

    Freed the deleted head

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,9 +8,12 @@
 
 int pop_listint(listint_t **head)
 {
+	listint_t *free_head;
 	int head_n;
 
+	free_head = *head;
 	head_n = (*head)->n;
 	*head = (*head)->next;
+	free(free_head);
 	return (head_n);
 }
