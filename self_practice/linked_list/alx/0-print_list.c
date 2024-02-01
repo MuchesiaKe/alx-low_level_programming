b72ddd17babc2bdcdf7cb5e9bb99c00b15b62991
Muchesia <muchesiawallace@gmail.com>
2024-02-01 09:14:14 +0300

    prints all the elements of a list

---
+++ self_practice/linked_list/alx/0-print_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while(h)
+	{
+		if (!(h->str))
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
