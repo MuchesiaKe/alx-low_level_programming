2dcb197a580ad7b7f6e551ec04a7ef3b91968df0
Muchesia <muchesiawallace@gmail.com>
2024-02-05 14:11:04 +0300

    modified code

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,26 @@
 #include "main.h"
 
 /**
-*puts2 - prints a string followed by a new line
-*
-*@str : string to print
+*puts2 - prints every other character of a string starting with the first character
+*@str: string to pring alternate characters
 **/
 
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
-		_putchar(str[i]);
+	i = 0;
+	while (*str)
+	{
+		if (i % 2 == 1)
+		{
+			i++;
+			str++;
+			continue;
+		}
+		_putchar(*str);
+		str++;
+		i++;
+	}
 	_putchar('\n');
 }
